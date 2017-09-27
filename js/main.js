b4af348a05a8e53ca46c0af3fb74e02e5cdaee37
Leah Birch <goldbeat98@gmail.com>
2017-09-27 15:03:52 +0200

    Third change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 //Project 9: JS
 
-const alert = document.querySelector('#alert'); 
+
+const alert = document.createElement('div');
+
+alert.className = 'alert-box';
+
+const p = document.createElement('p');
+
+const span = document.createElement('span');
+
+const alertText = document.createTextNode('Alert!');
+
+span.appendChild(alertText);
+
+p.appendChild(span);
+
+const alertMessage = document.createTextNode(' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellen...');
+
+p.appendChild(alertMessage);
+
+alert.appendChild(p);
+
+const cancelButton = document.createElement('span');
+
+cancelButton.className = 'cancel-button';
+
+const buttonSymbol = document.createTextNode('X');
+
+cancelButton.appendChild(buttonSymbol);
+
+alert.appendChild(cancelButton);
+
+const alertBox = document.querySelector('#alert').appendChild(alert);
+
+
