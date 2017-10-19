feb74155f6c8120e3db0f7a0a0d7e6a1c766346a
Leah Birch <goldbeat98@gmail.com>
2017-10-19 12:47:08 +0200

    fixed widget

--- js/widget.js
+++ js/widget.js
@@ -1,51 +1,77 @@
 //SOCIAL STATS
-var socialStats = new XMLHttpRequest();
+const socialStats = new XMLHttpRequest();
 socialStats.onreadystatechange = function () {
 	if (socialStats.readyState === 4 && socialStats.status === 200)	{
-		var stats = JSON.parse(socialStats.responseText);
-		var socialHTML = '<ul>';
+		const stats = JSON.parse(socialStats.responseText);
+		let socialHTML = '<ul class="social-stats">';
 		for (let i = 0; i < stats.length; i += 1)	{
 			socialHTML += "<li>";
-			socialHTML += '<img src="' + stats.icon + '" alt="icon">';
-			socialHTML += '<span>' + stats.media + '</span>';
-			socialHTML += '<span>' + stats.status + '</span>' + '</li>';
+        socialHTML += '<img src="' + stats[i].icon + '" alt="icon">';
+        socialHTML += '<div>';
+          socialHTML += '<span><b>' + stats[i].media + '</b></span>';
+          socialHTML += '<span>' + stats[i].status + '</span>';
+        socialHTML += '</div>';
+      socialHTML += '</li>';
 		}
 		socialHTML += '</ul>';
 		document.getElementById('socialStats').innerHTML = socialHTML;
 	}
 };
-socialStats.open('GET', '../js/socialStats.json', true);
+socialStats.open("GET", "../data/socialStats.json");
 socialStats.send();
 
-//////////////////
-//////////////////
-//////////////////
 
 //NEW MEMBERS WIDGET
-var newMembers = new XMLHttpRequest();
+const newMembers = new XMLHttpRequest();
 newMembers.onreadystatechange = function () {
 	if (newMembers.readyState === 4 && newMembers.status === 200) {
-		var members = JSON.parse(newMembers.responseText);
-		var memberHTML = '<ul class="new-members">';
+		const members = JSON.parse(newMembers.responseText);
+		let memberHTML = '<ul class="new-members">';
 		for (let i = 0; i < members.length; i += 1) {
 			memberHTML += '<li>';
-				memberHTML += '<img src="' + members[i].picture + '">';
+				memberHTML += '<img src="' + members[i].picture + '" alt="new member">';
         memberHTML += '<div>';
-				memberHTML += '<span><b>' + members[i].name;
-				memberHTML += '</b></span>';
-				memberHTML += '<p><a href="mailto:';
-				memberHTML += members[i].email + '">';
-				memberHTML += members[i].email;
-				memberHTML += '</a>';
-				memberHTML += '<span>';
-				memberHTML += members[i].date;
-				memberHTML += '</span></p>';
+          memberHTML += '<span><b>' + members[i].name;
+          memberHTML += '</b></span>';
+          memberHTML += '<p>';
+            memberHTML += '<a href="mailto:';
+              memberHTML += members[i].email + '">';
+              memberHTML += members[i].email;
+            memberHTML += '</a>';
+            memberHTML += '<span>';
+            memberHTML += members[i].date;
+            memberHTML += '</span>';
+          memberHTML += '</p>';
         memberHTML += '</div>';
 			memberHTML += '</li>';
 		}
 		memberHTML += '</ul>';
 		document.getElementById('newMembers').innerHTML = memberHTML;
 	}
-}
-newMembers.open('GET', '../js/new-member-data.json', true);
+};
+newMembers.open("GET", "../data/new-member.json");
 newMembers.send();
+
+
+//RECENT ACTIVITY WIDGET
+
+const recentActivity = new XMLHttpRequest();
+recentActivity.onreadystatechange = function () {
+  if (recentActivity.readyState === 4 && recentActivity.status === 200) {
+    const activity = JSON.parse(recentActivity.responseText);
+    let activityHTML = '<ul class="recent-activity">';
+    for (let i = 0; i < activity.length; i += 1)  {
+      activityHTML += '<li>';
+        activityHTML += '<img src="' + activity[i].img + '" alt="member activity">';
+        activityHTML += '<p><b>' + activity[i].name + '</b>';
+        activityHTML += activity[i].sentence + '</p>';
+        activityHTML += '<a href="#"><span>' + activity[i].arrow + '</span></a>';
+      activityHTML += '</li>';
+    }
+    activityHTML = '</ul>';
+    document.getElementById('recent-activity').innerHTML = activityHTML;
+  }
+};
+recentActivity.open("GET", "../data/recent-activity.json");
+recentActivity.send();
+
